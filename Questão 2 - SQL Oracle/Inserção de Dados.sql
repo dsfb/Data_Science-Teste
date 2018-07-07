e9c6f480e513c5e1ad712ac5c4b2036b83f5721f
Daniel Willians Ignacio de Souza <daniel2wis@gmail.com>
2018-07-07 00:09:21 -0300

    Alterando dados Exercicios

---
+++ Questão 2 - SQL Oracle/Inserção de Dados.sql
@@ -0,0 +1,16 @@
+﻿INSERT INTO ALUNOS(RA, NOME, ENDERECO, CIDADE)VALUES (1,'Daniel Willians','Eugênio de Melo','São José dos Campos');
+INSERT INTO ALUNOS(RA, NOME, ENDERECO, CIDADE)VALUES (2,'Isabella Bragion','Santana','São José dos Campos');
+INSERT INTO ALUNOS(RA, NOME, ENDERECO, CIDADE)VALUES (3,'Lucas Domingos','Eugênio de Melo','São José dos Campos');
+INSERT INTO ALUNOS(RA, NOME, ENDERECO, CIDADE)VALUES (4,'Diego Willians','Costinha','São José dos Campos');
+INSERT INTO ALUNOS(RA, NOME, ENDERECO, CIDADE)VALUES (5,'Larissa Mayara','Santa Inês','São José dos Campos');
+INSERT INTO ALUNOS(RA, NOME, ENDERECO, CIDADE)VALUES (6,'Alexandre Junior','Parque Res. Maria Elmira','Caçapava');
+INSERT INTO ALUNOS(RA, NOME, ENDERECO, CIDADE)VALUES (7,'Clauber','Jardim Julieta','Taubaté');
+INSERT INTO ALUNOS(RA, NOME, ENDERECO, CIDADE)VALUES (8,'Maria de Fatima','Santana','São Paulo');
+INSERT INTO ALUNOS(RA, NOME, ENDERECO, CIDADE)VALUES (9,'Emanuela','Jardim Paulista','São José dos Campos');
+INSERT INTO ALUNOS(RA, NOME, ENDERECO, CIDADE)VALUES (10,'Henrique','Vila Ema','São José dos Campos');
+
+INSERT INTO DISCIPLINAS(COD_DISC, NOME_DISC, CARGA_HOR)VALUES (111,'Matemática',40);
+INSERT INTO DISCIPLINAS(COD_DISC, NOME_DISC, CARGA_HOR)VALUES (112,'Algoritimo',80);
+INSERT INTO DISCIPLINAS(COD_DISC, NOME_DISC, CARGA_HOR)VALUES (113,'Linguagem de Programação',80);
+INSERT INTO DISCIPLINAS(COD_DISC, NOME_DISC, CARGA_HOR)VALUES (114,'Planejamento Estrategico',80);
+
