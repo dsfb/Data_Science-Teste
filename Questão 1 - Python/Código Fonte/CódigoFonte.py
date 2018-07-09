d73fd6a22f06475925a3c45a0a7c008ad19661c1
Daniel Willians Ignacio de Souza <daniel2wis@gmail.com>
2018-07-09 04:51:10 -0300

    Questão 1 e 3 - Finalizando

--- Questão 1 - Python/Código Fonte/CódigoFonte.py
+++
@@ -1,2 +0,0 @@
-import pandas as pd
-train_df = pd.read_csv('house_infos.csv')
--- Questão 1 - Python/Código Fonte/temp.py
+++ Questão 1 - Python/Código Fonte/temp.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-Anaconda
 """
+#1 Ler o arquivo house_infos.csv na forma de um dataframe.
 import pandas as pd
 house = pd.read_csv('house_infos.csv')
-print(house)
+#b) Neste dataframe, qual a quantidade de casas que possuem “LotArea” > 5000 e “CentralAir” = N?
+cont =0
+for i in range (0,1459):
+        if ((house.loc[i][1]>5000) and (house.loc[i][7]=='N')):
+            cont=cont+1
+print('Quantidade de casas que possuem “LotArea” > 5000 e “CentralAir” = {}'.format(cont))
+#c) Filtrar dataframe para conter apenas as linhas que tenham “GrLivArea” > 2000
+copy = house.filter(items=['GrLivArea'])
+copy = copy.loc[copy['GrLivArea']>2000]
